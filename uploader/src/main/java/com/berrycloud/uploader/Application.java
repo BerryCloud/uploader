13a9824ed7bee7275f3fb196b531b619315ea3ab
tom <tom@berrycloud.co.uk>
2014-08-19 11:39:10 +0100

    set upload limit to 4GB

--- uploader/src/main/java/com/berrycloud/uploader/Application.java
+++ uploader/src/main/java/com/berrycloud/uploader/Application.java
@@ -1,14 +1,13 @@
 package com.berrycloud.uploader;
 
+import javax.servlet.MultipartConfigElement;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.embedded.MultipartConfigFactory;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import javax.servlet.MultipartConfigElement;
 
 @Configuration
 @EnableAutoConfiguration
@@ -18,6 +17,9 @@ public class Application {
 	@Bean
 	MultipartConfigElement multipartConfigElement() {
 		MultipartConfigFactory factory = new MultipartConfigFactory();
+
+		factory.setMaxFileSize("4GB");
+		factory.setMaxRequestSize("4GB");
 
 		return factory.createMultipartConfig();
 	}
